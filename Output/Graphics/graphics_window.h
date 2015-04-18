58f2e7c398d3113690d3a9bb54bc819086091e0f
Kenneth Smith <ksmi01@nmt.edu>
2015-04-18 21:44:11 +0000

    Working again

--- Output/Graphics/graphics_window.h
+++ Output/Graphics/graphics_window.h
@@ -19,9 +19,11 @@ WINDOW* create_window(GUI* g,char* title, int bgColor);
 
 void set_window_icon(GUI* g, WINDOW* w,char* filename);//TODO
 
-void set_window_size(GUI* g,WINDOW* win, int height, int width);//TODO
+void set_window_size(GUI* g,WINDOW* win, int height, int width);
 
 void add_widget_to_window(WINDOW* win, WIDGET* w);//TODO
+
+void update_window_widget(WINDOW* win, WIDGET* w);//TODO
 
 void set_window_visible(GUI* g,WINDOW* win,int visible);
 
