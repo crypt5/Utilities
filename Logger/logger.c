3a6bfd2357799a8aa15d386c192b6017081f0e8b
Kenneth Jay Smith <ksmi01@nmt.edu>
2016-02-19 23:02:56 -0700

    Removed debug statements and updated the documentation.
    All bugs I am aware of have been fixed

--- Logger/logger.c
+++ Logger/logger.c
@@ -48,9 +48,9 @@ void* loop(void* data)
         }
         runner = log->run;
         pthread_mutex_unlock(&log->lock);
-        /* Micro * milli */
         usleep(update);
     }
+    //Shutting down So dump buffers
     pthread_mutex_lock(&log->lock);
     if (log->cur_buffer == 2) {
         if (log->buf_2_pos != 0) {
@@ -67,9 +67,14 @@ void* loop(void* data)
 
 /**
  * Creates a new log file with the set filename
- * @param filename - The filename and path to use
+ * @param filename - The path a filename to use
  * @param buffer_size - The desired size of the buffer. a value of 0
  * results in the default 4k buffer
+ * @param update_int - The amount of time(micro Seconds) the writing
+ * thread should wait between runs. This value should be as long as
+ * possible, but not so long that both buffers fill up. this will
+ * cause the logger_log function to block the caller while it dumps
+ *  a buffer
  * @return LOGGER* on success, NULL if malloc,
  * mutex, or thread creation failed
  */
@@ -120,11 +125,14 @@ LOGGER *logger_init(const char* filename, unsigned int buffer_size,
 }
 
 /**
- * Adds the message to the logger queue
+ * Adds the message to the logger buffer. The logger will
+ * append the message to the date and time before it writes
+ * the information to file.
  * @param log - The logger to use
- * @param message - The message to add
- *    the logger will add the time and data to the
- *    beginning of the line.
+ * @param message - The message to add. If the length of
+ * the message is greater then 1000 characters the logger
+ * will be forced to allocate a different buffer to use.
+ * This will slow down the calling thread.
  * @return -1 if log is NULL, -2 if message + time and date
  *  is longer then 1024 and malloc fails, and 0 on success
  */
@@ -250,23 +258,24 @@ int logger_log(LOGGER *log, const char* message)
 }
 
 /**
- * Shuts down the logger and adds the remaining
- * lines to the log file
+ * Shuts down the logger and dumps the buffers to file
  * @param log - Logger to shutdown
- * @return 0 on success
+ * @return 0 on success, error from fclose if the
+ * file did not close correctly
  */
 int logger_shutdown(LOGGER *log)
 {
+    int re = 0;
     pthread_mutex_lock(&log->lock);
     log->run = 0;
     pthread_mutex_unlock(&log->lock);
     pthread_join(log->thread, NULL);
-    fclose(log->log);
+    re = fclose(log->log);
     pthread_mutex_destroy(&log->lock);
     free(log->buffer1);
     free(log->buffer2);
     free(log);
 
-    return 0;
+    return re;
 
 }
