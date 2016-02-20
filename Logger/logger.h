1c3acad0a018a88dd84f94c1d43e3b39fe62766f
Kenneth Jay Smith <ksmi01@nmt.edu>
2016-02-19 21:08:11 -0700

    Initial Buffered Version Commit.
    Bug hunting to follow

--- Logger/logger.h
+++ Logger/logger.h
@@ -6,10 +6,12 @@ typedef struct logger_t LOGGER;
 /**
  * Creates a new log file with the set filename
  * @param filename - The filename and path to use
+ * @param buffer_size - The desired size of the buffer. a value of 0
+ * results in the default 4k buffer
  * @return LOGGER* on success, NULL if malloc,
  * mutex, or thread creation failed
  */
-LOGGER *logger_init(char* filename);
+LOGGER *logger_init(const char* filename, unsigned int buffer_size);
 
 /**
  * Adds the message to the logger queue
@@ -20,7 +22,7 @@ LOGGER *logger_init(char* filename);
  * @return -1 if log is NULL, -2 if line malloc fails,
  * -3 if enqueue fails, and 0 on success
  */
-int logger_log(LOGGER *log, char* message);
+int logger_log(LOGGER *log, const char* message);
 
 /**
  * Shuts down the logger and adds the remaining
